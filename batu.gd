a01468ddca5666bf9310fd9fb9dead8ecb4ae847
peekoy <mhmmdfaiz160@gmail.com>
2026-09-03 16:29:01 +0700

    feat: batu interaction

---
+++ batu.gd
@@ -0,0 +1,25 @@
+extends Area2D
+
+var player_in_range: bool = false
+
+@onready var label: Label = $"../LabelBatu"
+
+func _ready() -> void:
+    label.text = ""
+
+    body_entered.connect(_on_body_entered)
+    body_exited.connect(_on_body_exited)
+
+func _on_body_entered(body: Node2D) -> void:
+    if body.name == "Player":
+        player_in_range = true
+        label.text = "Batu"
+
+func _on_body_exited(body: Node2D) -> void:
+    if body.name == "Player":
+        player_in_range = false
+        label.text = ""
+
+func _process(_delta: float) -> void:
+    if player_in_range and Input.is_key_pressed(KEY_E):
+        print("Batu")
